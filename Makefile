8404414245fea870c092a1302f4461e6ae281b2a
Vadim Godunko <vgodunko@gmail.com>
2024-06-05 11:13:11 +0400

    Add test.

--- Makefile
+++ Makefile
@@ -1,8 +1,25 @@
 
-all:
-	alr build
+all: build
 	./bin/gfx-driver
 	viewnior out.ppm
 
+gimp: build
+	./bin/gfx-driver
+	gimp out.ppm
+
+check: build
+	./testsuite/bin/test1
+	diff -u testsuite/test1.ppm out.ppm
+
+build:
+	alr build -- -cargs -O0
+
 clean:
 	rm -rf .objs bin config alire
+
+gdb: build
+	gdb --command=gdbinit ./bin/gfx-driver
+
+asm:
+	alr build -- -c -u -f gfx-painter.adb -cargs -S -O2
+	alr build -- -c -u -f gfx-painter.adb --target=arm-eabi --RTS=light-cortex-m7f -cargs -S -O2
---
+++ testsuite/source/test1.adb
@@ -0,0 +1,27 @@
+--
+--  Copyright (C) 2024, Vadim Godunko <vgodunko@gmail.com>
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+--
+
+with GFX.Painter;
+with GFX.PPM;
+
+procedure Test1 is
+   C : constant GFX.RGBA := GFX.To_RGBA (0, 255, 0, 0);
+
+begin
+   GFX.PPM.Set_Size (130, 130);
+
+   GFX.Painter.Draw_Line (15.0, 50.0, 16.0, 70.0, C);
+   GFX.Painter.Draw_Line (34.5, 50.0, 35.5, 70.0, C);
+   GFX.Painter.Draw_Line (55.0, 49.5, 56.0, 69.5, C);
+   GFX.Painter.Draw_Line (74.5, 49.5, 75.5, 69.5, C);
+
+   GFX.Painter.Draw_Line (16.0, 80.0, 15.0, 100.0, C);
+   GFX.Painter.Draw_Line (35.5, 80.0, 34.5, 100.0, C);
+   GFX.Painter.Draw_Line (56.0, 79.5, 55.0, 99.5, C);
+   GFX.Painter.Draw_Line (75.5, 79.5, 74.5, 99.5, C);
+
+   GFX.PPM.Save ("out.ppm");
+end Test1;
