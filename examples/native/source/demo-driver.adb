b46e2f0818bf60ecffa6291ffadbaa47289707fd
Vadim Godunko <vgodunko@gmail.com>
2024-06-29 10:16:43 +0400

    Stub for native demo.

---
+++ examples/native/source/demo-driver.adb
@@ -0,0 +1,10 @@
+--
+--  Copyright (C) 2024, Vadim Godunko <vgodunko@gmail.com>
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+--
+
+procedure Demo.Driver is
+begin
+   null;
+end Demo.Driver;
